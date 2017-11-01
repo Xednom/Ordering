249b6de82e66afbdb9f36590bc1149b563bc3fae
Monde <xednom@gmail.com>
2017-11-02 00:35:25 +0800

    Update: updated wsgi.py

--- systems/wsgi.py
+++ systems/wsgi.py
@@ -8,15 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-"""
+
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
-
-application = get_wsgi_application()
-"""
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
