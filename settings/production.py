d529ea8103983961190e585cf037cc9699459464
Monde <xednom@gmail.com>
2017-11-03 22:53:59 +0800

    Update: changed STATICFILES_DIRS in prod

--- settings/production.py
+++ settings/production.py
@@ -195,7 +195,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'theme/img')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'settings'),
  )
 
 # Simplified static file serving.
