fe2bbcd66d0f89a23892c5a3cfa357ab0c118850
xednom <xednom@gmail.com>
2017-11-04 11:21:42 +0800

    Update: changes on name of text in the navbar, removed the comment for STATICFILES_STORAGE in prod.

--- systems/wsgi.py
+++ systems/wsgi.py
@@ -6,13 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 import os
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
