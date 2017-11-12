31aca5b8a8c9f2e5055c910ac0354ed238ad7efa
xednom <xednom@gmail.com>
2017-11-12 18:42:35 +0800

    Update: commented out whitenoise storage.

--- settings/production.py
+++ settings/production.py
@@ -201,7 +201,7 @@ STATICFILES_DIRS = (
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Update database configuration with $DATABASE_URL.
